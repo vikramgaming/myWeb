d6c7d276203f13c4f09d41e4a81d0b782311657c
vikramgaming <update>
2025-09-25 20:51:29 +0700

    update

--- Battle-Royale/copy.js
+++ Battle-Royale/copy.js
@@ -491,8 +491,8 @@ const canvas = document.getElementById("GameCanvas");
 const ctx = canvas.getContext("2d");
 const count = document.getElementById("entities"); //menghitung musuh
 
-const fire = new Audio("/play/audio/fire.ogg");
-const enemyFire = new Audio("/play/audio/fire.ogg");
+const fire = new Audio("Play/audio/fire.ogg");
+const enemyFire = new Audio("Play/audio/fire.ogg");
 const vol = document.getElementById("volume");
 const output = document.getElementById("output");
 
@@ -670,7 +670,7 @@ function start() {
             enemies[e].hp--;
             if (enemies[e].hp <= 0) {
               enemies.splice(e, 1);
-              killNotif("player", e);
+              killNotif("player", e)
               entities--;
             }
             break;
